c051aa2736e8aa134e64498200bcb6ee1dc5fabc
anyuruf <anyuruf@gmail.com>
2023-05-22 11:52:18 +0300

    get d3-force to work in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppLayoutComponent } from './app-layout/app-layout.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AngularMaterialModule } from './angular-material.modulte';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppLayoutComponent
+    AppLayoutComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, the Shiba Inu was originally bred for hunting.`;
+
+}
